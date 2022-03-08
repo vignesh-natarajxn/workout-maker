7cf33c4adc8946216b4c0f0d4b991d45521d7655
Vignesh Natarajan <viknhat@gmail.com>
2022-03-08 22:49:49 +0530

    mui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./Styles.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ExerciseDay } from "./models/exerciseDay";
 import UIExerciseWeek from "./UIExerciseWeek";
+import { Typography } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff8400'
+    },
+    secondary: {
+      main: '#191f27'
+    }
+
+  },
+  typography: {
+    fontFamily: 'Exo',
+    fontWeightLight: '400',
+    fontWeightRegular: '500',
+    fontWeightMedium: '600',
+    fontWeightBold: '700',
+  }
+})
 
 const EXERCISE_WEEK: ExerciseDay[] = [];
 
@@ -10,107 +31,128 @@ EXERCISE_WEEK.push(
   {
     id: "d1",
     name: "Day 1",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }]
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d2",
     name: "Day 2",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d3",
     name: "Day 3",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d4",
     name: "Day 4",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d5",
     name: "Day 5",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d6",
     name: "Day 6",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   },
   {
     id: "d7",
     name: "Day 7",
-    exercises: [{
-      name: "Exercise 1",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 1a",
-    },{
-      name: "Exercise 2",
-      timeBetween: 120,
-      sets: 3,
-      superset: "Exercise 2a",
-    }],
+    exercises: [
+      {
+        name: "Exercise 1",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 1a",
+      },
+      {
+        name: "Exercise 2",
+        timeBetween: 120,
+        sets: 3,
+        superset: "Exercise 2a",
+      },
+    ],
   }
 );
 
@@ -122,23 +164,19 @@ function App() {
   }, []);
 
   return (
-    <>
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+      <Typography
+        className="App-header"
+        variant="h5"
+        align="center"
+        color="textSecondary"
+        gutterBottom
+      >
         <img src={logo} className="App-logo" />
         <div>Workout Maker</div>
-      </header>
+      </Typography>
       <UIExerciseWeek exerciseWeek={exerciseWeek} />
-      {/* <header className="App-header">
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/app/UIExerciseWeek.tsx
+++ src/app/UIExerciseWeek.tsx
@@ -1,25 +1,48 @@
 import React from "react";
 import { ExerciseDay } from "./models/exerciseDay";
 import "./Styles.css";
-import Button from '@mui/material/Button';
+import { Box, Button, ButtonGroup } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 interface Props {
   exerciseWeek: ExerciseDay[];
 }
 
+const useStyles: any = makeStyles({
+  day: {
+    fontSize: 20,
+    "&:hover": {
+      backgroundColor: "#ff8400",  
+    },
+    marginTop: 30,
+  },
+});
+
 export default function UIExerciseWeek({ exerciseWeek }: Props) {
+  const classes = useStyles();
 
   const daySelect = () => {
-    console.log("yolo")
-  }
+    console.log("yolo");
+  };
 
   return (
-    <div className="Exercise-week">
-      {exerciseWeek.map((exerciseDay) => (
-        <Button variant="contained" onClick={daySelect} color="primary">
-          <h1 className="Exercise-day-content">{exerciseDay.name}</h1>
-        </Button>
-      ))}
-    </div>
+    <Box textAlign='center'>
+      <ButtonGroup>
+        {exerciseWeek.map((exerciseDay) => (
+          <Button
+            key={exerciseDay.id}
+            onClick={daySelect}
+            className={classes.day}
+            variant="contained"
+            color="secondary"
+          >
+            <h1>{exerciseDay.name}</h1>
+          </Button>
+        ))}
+      </ButtonGroup>
+      {<Box>
+
+      </Box>}
+    </Box>
   );
 }
