370ebfcc03b4036c3d6ed9551feff046bad96fc3
Vignesh Natarajan <viknhat@gmail.com>
2022-03-09 16:04:42 +0530

    hover incomplete

--- src/app/UIExerciseWeek.tsx
+++ src/app/UIExerciseWeek.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExerciseDay } from "./models/exerciseDay";
 import "./Styles.css";
-import { Box, Button, ButtonGroup } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 interface Props {
@@ -10,39 +17,68 @@ interface Props {
 
 const useStyles: any = makeStyles({
   day: {
-    fontSize: 20,
+    fontSize: 10,
     "&:hover": {
-      backgroundColor: "#ff8400",  
+      border: "2px solid #ff8400",
     },
     marginTop: 30,
+    height: 300,
+    width: 170,
   },
 });
 
+interface IHover {
+  [name: string]: boolean
+}
+
+let hoverInitial: IHover = {};
+
 export default function UIExerciseWeek({ exerciseWeek }: Props) {
+  exerciseWeek.map((exerciseDay) => {
+    hoverInitial[exerciseDay.id]= false;
+  });
+  
   const classes = useStyles();
+  const [hover, setHover] = useState(hoverInitial);
+  
+  const hoverHandler = (value: boolean, exname: string) => {
+    setHover((data)=> {
+    data[exname]=value
+    return data})
+    setHover((data)=> data)
+    console.log(hover)
+  };
 
   const daySelect = () => {
     console.log("yolo");
   };
 
   return (
-    <Box textAlign='center'>
-      <ButtonGroup>
-        {exerciseWeek.map((exerciseDay) => (
+    <Box textAlign="center">
+      {exerciseWeek.map((exerciseDay) => {
+        return (
           <Button
+            onMouseEnter={() => hoverHandler(true, exerciseDay.id)}
+            onMouseLeave={() => hoverHandler(false, exerciseDay.id)}
             key={exerciseDay.id}
             onClick={daySelect}
             className={classes.day}
             variant="contained"
             color="secondary"
           >
-            <h1>{exerciseDay.name}</h1>
+            {(hover[exerciseDay.id]===true)? (
+              <List>
+                {exerciseDay.exercises.map((exercise) => (
+                  <ListItemText primary={exercise.name} />
+                ))}
+              </List>
+            ) : (
+              <h1>{exerciseDay.name}</h1>
+            )}
           </Button>
-        ))}
-      </ButtonGroup>
-      {<Box>
-
-      </Box>}
+        );
+      })}
+      {<Box></Box>}
     </Box>
   );
 }
