22d198d9264045052720de083ce513cfd78014ed
Vignesh Natarajan <viknhat@gmail.com>
2022-03-21 13:48:24 +0530

    logo

--- src/app/layout/startExercises/UIBarDrawer.tsx
+++ src/app/layout/startExercises/UIBarDrawer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../logo.svg";
+import logo from "../../logo.png";
 import "../../Styles.css";
 
 // Components
