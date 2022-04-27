2426d5a7d701125aada6f55f58f280a092af72d0
Vignesh Natarajan <viknhat@gmail.com>
2022-04-27 20:20:23 +0530

    audio

--- src/app/layout/startExercises/UIExerciseDay/ExerciseDayMain.tsx
+++ src/app/layout/startExercises/UIExerciseDay/ExerciseDayMain.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useSound from "use-sound";
 
 // Components
 import Timer from "./ExerciseDayMain/Timer";
@@ -10,6 +11,7 @@ import { ExerciseDay } from "../../../models/exerciseDay";
 // Material UI
 import { Button, Fade, ListItem, Typography } from "@mui/material";
 
+import boopSfx from '../../../done.mp3';
 /************************************************************************************************/
 
 interface Props {
@@ -30,10 +32,12 @@ export default function UIExerciseDayMain({
   const [showTimer, setShowTimer] = useState<boolean>(false);
   const [superset, setSuperset] = useState<boolean>(false);
   const [set, setSet] = useState<number>(1);
+  const [play] = useSound(boopSfx);
 
   //|||||||||||||||||||||||||||||||||||||||||||
 
   const nextExerciseHandler = (opr: string) => {
+    play();
     if (opr === "timer") {
       if (currentDay.exercises[currentExercise].superset && !superset) {
         setSuperset(true);
@@ -66,6 +70,7 @@ export default function UIExerciseDayMain({
   return (
     <Fade in timeout={{ enter: 700 }}>
       <div>
+      <audio src="/done.mp3" autoPlay/>
         {currentExercise === 1000 && (
           <Completed
             currentDay={currentDay}
